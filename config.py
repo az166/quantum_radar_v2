1410b698f8494df86c0294f8789fa5a6d52976e9
az166 <57324005+az166@users.noreply.github.com>
2026-07-15 12:16:30 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# Set pencarian cepat O(1) untuk menyaring koin hitam (Blacklist)
+COIN_BLACKLIST = {'UPUSDT', 'DOWNUSDT', 'BUSDUSDT', 'USDCUSDT', 'FDUSDUSDT', 'EURUSDT'}
+
+# TELEGRAM BOT CONFIGURATION
+TELEGRAM_TOKEN = "8979605471:AAGToVU4-bkZIPi9DeqhE8CCNYIp-bM3Bvs"
+TELEGRAM_CHAT_ID = "@cryptoradar_quantum"
+
+# Engine configuration
+CACHE_TTL_SECONDS = 120
